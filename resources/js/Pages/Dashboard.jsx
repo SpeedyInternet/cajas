9c7708cf11e4b00ad59415395799073e2fbd6faf
Jorge Ibarra <105680706+JIbarra1117@users.noreply.github.com>
2024-08-07 18:01:42 -0500

    Mejora de mensajes y front

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -187,7 +187,7 @@ export default function Dashboard({ auth }) {
           icon: "success"
         });
       }
-    });;
+    });
 
     if (confirmacion) {
       // Lógica para registrar el pago
